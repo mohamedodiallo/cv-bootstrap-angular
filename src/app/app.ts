b0375a4be486b50589b704f75809bcea3c2764dd
Mohamed Oureychiyou Diallo <modiallo@ept.sn>
2025-12-23 22:51:23 +0000

    Partie 4 - Intégration Angular avec composant entête

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Entete } from './entete/entete';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Entete],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('cv_bootstrap');
+  title = 'cv_bootstrap';
 }
---
+++ src/app/entete/entete.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'entete-cv',
+  imports: [],
+  templateUrl: './entete.html',
+  styleUrl: './entete.css',
+})
+export class Entete {
+
+}
